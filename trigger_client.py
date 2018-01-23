3664864c99e5634db40107ecba42ba476c01aaca
Rishikanth <rishikanthc@gmail.com>
2018-01-23 05:09:10 +0000

    Adding mqtt trigger

---
+++ trigger_client.py
@@ -0,0 +1,16 @@
+import paho.mqtt.client as mqtt
+
+# The callback for when the client receives a CONNACK response from the server.
+def on_connect(client, userdata, flags, rc):
+    #print("Connected with result code "+str(rc))
+    client.subscribe("ledStatus")
+
+# The callback for when a PUBLISH message is received from the server.
+def on_message(client, userdata, msg):
+    print(msg.topic+" "+str(msg.payload))
+
+client = mqtt.Client()
+client.on_connect = on_connect
+client.on_message = on_message
+client.connect("server.local", 1883, 60)
+client.loop_forever()
