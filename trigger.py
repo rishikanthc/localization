d91add0a56ea0c284d8110ae465da28e893791e8
rishikanth <rishikanthc@gmail.com>
2018-01-24 02:05:14 -0800

    Adding training set

--- trigger.py
+++ trigger.py
@@ -1,6 +1,6 @@
 import paho.mqtt.publish as publish
 import time
-print("Sending 0...")
-publish.single("ledStatus", "2", hostname="server.local")
-time.sleep(1)
-print("Sending 1...")
+import sys
+
+print("sending {}".format(sys.argv[1]))
+publish.single("ledStatus", sys.argv[1], hostname="server.local")
