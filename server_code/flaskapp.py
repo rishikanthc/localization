52a9d102a9fe7432528ee8c3ddb6ca0c9d8fb0a7
rishikanth <rishikanthc@gmail.com>
2018-04-07 14:02:37 -0700

    server code updates

--- server_code/flaskapp.py
+++ server_code/flaskapp.py
@@ -2,19 +2,94 @@ from flask import Flask, jsonify, request, make_response
 import logging
 from logging.handlers import RotatingFileHandler
 import paho.mqtt.publish as publish
+import pickle
+import math
+import operator
+
+
 app = Flask(__name__)
 
 ### Global Variables ###
 message = "hello"
+XY_train = []
 rssi_1 = 0
 rssi_2 = 0
 rssi_3 = 0
 
+
+
+################# KNN #######################
+def load_data(file_name):
+    f = open(file_name)
+    var = pickle.load(f)
+    f.close()
+    return var
+
+def dist_func(train_point, test_point):
+    dist = (train_point[0] - test_point[0])**2
+    dist += (train_point[1] - test_point[1])**2 
+    dist += (train_point[2] - test_point[2])**2
+    return math.sqrt(dist)
+                    
+def getNeighbors(trainingSet, testInstance, k):
+    global XY_train
+    distances = []
+    for x in range(len(XY_train)):
+        dist = dist_func(testInstance,XY_train[x])
+        distances.append((XY_train[x], dist))
+    distances.sort(key=operator.itemgetter(1))
+    neighbors = []
+    for x in range(k):
+        neighbors.append(distances[x][0])
+    return neighbors
+
+def getResponse(neighbors):
+    classVotes = {}
+    for x in range(len(neighbors)):
+        response = neighbors[x][-1]
+        if response in classVotes:
+            classVotes[response] += 1
+        else:
+            classVotes[response] = 1
+    sortedVotes = sorted(classVotes.iteritems(), key=operator.itemgetter(1), reverse=True)
+    return sortedVotes[0][0]
+
+
+############# APIs #########################
+
 @app.route('/')
 def hello_world():
-    global rssi_1
-    return "Values: " + str(rssi_1) + str(rssi_2) + str(rssi_3) 
+    global rssi_1, XY_train
+    #XY_train = load_data("/home/pi/localization/X_Y_train.data")
+    XY_train = load_data("/home/pi/localization/new_train.data")
+    #XY_train = load_data("/home/pi/localization/new1_train.data")
+    return "Values: {} {} {}".format(str(rssi_1), str(rssi_2), str(rssi_3)) 
     #return 'Hello from Flask!'
+
+@app.route('/location')
+def findLocation():
+    global rssi_1, rssi_2, rssi_3
+    loc_area = "Location unavailable"
+
+    if rssi_1 == 0 or rssi_2 == 0 or rssi_3 == 0:
+        return jsonify({'area':loc_area, 'rssi1':rssi_1, 'rssi2':rssi_1, 'rssi3':rssi_3})
+    else:
+        neighbors = getNeighbors(XY_train, (rssi_1, rssi_2, rssi_3), 8)
+        loc = getResponse(neighbors)
+
+    if loc == 'b':
+        loc_area = "bedroom"
+    elif loc == 'k':
+        loc_area = "kitchen"
+    elif loc == 'h':
+        loc_area = "hallway"
+    elif loc == 'l':
+        loc_area = "living room"
+    else:
+        loc_area = "Unknown"
+
+    return jsonify({'area':loc_area, 'rssi1':rssi_1, 'rssi2':rssi_1, 'rssi3':rssi_3})
+
 
 @app.route('/start')
 def start_train():
